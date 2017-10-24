7063ff06d009e473182e28f8e4e26b0a71519c25
wamisnet <wamiwami@live,jp>
2017-10-25 00:47:15 +0900

    NefryBeaconライブラリ追加

---
+++ libraries/Nefry_Beacon/src/NefryBeacon.h
@@ -0,0 +1,65 @@
+// Copyright 2015-2016 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef _NEFRY_BEACON_H_
+#define _NEFRY_BEACON_H_
+
+#include "sdkconfig.h"
+
+#if defined(CONFIG_BT_ENABLED) && defined(CONFIG_BLUEDROID_ENABLED)
+
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "bt.h"
+
+#include "Arduino.h"
+
+struct ble_gap_adv_params_s;
+
+class NefryBeacon {
+    public:
+
+        NefryBeacon(void);
+        ~NefryBeacon(void);
+
+        /**
+         * Start BLE Advertising
+         *
+         * @param[in] localName  local name to advertise
+         *
+         * @return true on success
+         *
+         */
+        bool begin(uint8_t uuid[16], uint16_t major, uint16_t minor);
+
+        /**
+         * Stop BLE Advertising
+         *
+         * @return none
+         */
+        bool end(void);
+
+    private:
+        String local_name;
+    private:
+
+};
+
+#endif
+
+#endif
